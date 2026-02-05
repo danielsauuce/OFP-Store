06d5dad7be0acbc0fd2fcd728ea4b0b3b414c3a6
Daniel <danieladeyd77@gmail.com>
2026-02-05 19:19:56 +0000

    auth controller and routes modidified, forgot password with email verification implemented

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,19 +5,25 @@ import {
   changePassword,
   checkAuth,
   logoutUser,
+  getCurrentUser,
+  forgotPassword,
+  resetPassword,
 } from '../controllers/authController.js';
 import { authenticate } from '../middleware/checkAuthMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
-
 router.post('/login', loginUser);
-
-router.post('/reset-password', authenticate, changePassword);
-
 router.get('/logout', logoutUser);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
 
-router.get('/check-auth', authenticate, checkAuth);
+// protected routes
+router.use(authenticate);
+
+router.post('/reset-password', changePassword);
+router.get('/check-auth', checkAuth);
+router.get('/me', getCurrentUser);
 
 export default router;
