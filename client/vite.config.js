b90abddbb8538e8aac74807f9c5d643e12c96fe3
Daniel Omogbolahan Adeyemi <danieladeyd77@gmail.com>
2026-03-13 12:51:58 +0000

    vit bundle analyzer implemnted for optimization

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { analyzer } from 'vite-bundle-analyzer';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    analyzer({
+      openAnalyzer: true,
+      summary: true,
+    }),
+  ],
 });
